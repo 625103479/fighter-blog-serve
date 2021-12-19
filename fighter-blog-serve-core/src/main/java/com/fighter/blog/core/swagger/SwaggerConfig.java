8b999eef1e60a3650fe3496953f36692f937bd79
jie.zhang <625103479@qq.com>
2021-12-20 00:11:21 +0800

    commit test

---
+++ fighter-blog-serve-core/src/main/java/com/fighter/blog/core/swagger/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.fighter.blog.core.swagger;
+
+/**
+ * @BelongsProject: fighter-blog-serve
+ * @BeloongsPackage: com.fighter.blog.core.swagger
+ * @ClassName: SwaggerConfig
+ * @CreateTime: 2021/12/20 0:10
+ * @author: jie.zhang
+ * @Email: 625103479@qq.com
+ * @Desc:
+ */
+public class SwaggerConfig {
+}
